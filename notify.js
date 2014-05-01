f0e65cf5220783395cb8df3bc5e2ef77854dc469
David Persson <davidpersson@gmx.de>
2014-05-01 13:41:28 +0200

    Rewrite.
    - Make it configurable.
    - Use CSS3 transition when possible.

--- notify.js
+++ notify.js
@@ -7,56 +7,107 @@
  * http://www.opensource.org/licenses/mit-license.php
  */
 (function($) {
-  var queue = $({});
 
-  var render = function(html, options, complete) {
-    var m = $('<div>').attr('class', 'message');
+  // Global configuration.
+  var config = {
+    // Selector of the container in which messages will be added.
+    container: '#messages',
+    element: '<div class="message hide"></div>',
 
-    if (options.level) {
-      m.addClass(level);
+    // Number of maximum pending messages, all other messages
+    // will be discarded to prevent flooding.
+    maxPending: 2,
+
+    // Base timeout until a message will be removed after it's shown.
+    timeout: 2100,
+
+    // Show function used when a message should appear; must return a
+    // promise which should be resolved when i.e. the show animation
+    // finished.
+    show: function($m) {
+      var dfr = new $.Deferred();
+
+      if (typeof Modernizr !== 'undefined' && Modernizr.csstransitions) {
+        $m.one('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', dfr.resolve);
+        $m.removeClass('hide');
+      } else {
+        $m.fadeIn(dfr.resolve);
+      }
+      return dfr.promise();
+    },
+
+    // Hide function used when a message should disappear; must return
+    // a promise which should be resolved when i.e. the hide animation
+    // finished.
+    hide: function($m) {
+      var dfr = new $.Deferred();
+
+      if (typeof Modernizr !== 'undefined' && Modernizr.csstransitions) {
+        $m.one('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', dfr.resolve);
+        $m.addClass('hide');
+      } else {
+        $m.fadeOut(dfr.resolve);
+      }
+      return dfr.promise();
     }
-    if (options.sticky) {
-      m.addClass('sticky');
-    }
-    m.hide();
-
-    m.html(html);
-    $('#messages').append(m);
-    m.show();
-
-    if (options.sticky) {
-      m.click(function(ev) {
-        $(this).remove();
-        complete();
-      });
-
-      // Do not continue and probably show next message.
-      return;
-    }
-
-    // Multiply the timeout for complex messages,
-    // giving comprehension time. Give 1s per 30chars over the first 25 chars.
-    options.timeout = 1000 * (($($.parseHTML(html)).text().length - 25) / 30);
-
-    setTimeout(function() {
-      $container.trigger('notify:remove', [m]);
-      m.fadeOut('fast', function() {
-        $(this).remove();
-        complete(); // Show next message in queue.
-      });
-    }, options.timeout);
   };
 
+  // Our own personal queue.
+  var queue = $({});
+
+  // Renders a message and calls complete callback after doing that.
+  var render = function(html, options, complete) {
+    var $m = $(config.element).html(html);
+
+    if (options.level) {
+      $m.addClass(level);
+    }
+    if (options.sticky) {
+      $m.addClass('sticky');
+    }
+    $(config.container).append($m);
+
+    if (options.sticky) {
+      $m.click(function(ev) {
+        config.hide(m).done(function() {
+          $m.remove();
+          complete();
+        });
+      });
+      return; // Do not continue and probably show next message.
+    }
+
+    // Multiply the timeout for complex messages, giving comprehension time.
+    // Give 1s per 30 chars over the first 25 chars.
+    options.timeout = 1000 * (($($.parseHTML(html)).text().length - 25) / 30);
+
+    config.show($m).done(function() {
+      setTimeout(function() {
+        config.hide($m).done(function() {
+          $m.remove();
+          complete(); // Show next message in queue.
+        });
+      }, options.timeout);
+    });
+  };
+
+  // Main plugin function. Allows for adding a notification and
+  // configuring the plugin globally.
   $.notify = function(html, options) {
+    // Allow setting global configuration for this plugin.
+    if (typeof html === 'object') {
+      config = $.extend(html, config);
+      return;
+    }
     options = $.extend(options || {}, {
       level: null,
       sticky: false,
-      timeout: 2100
+      timeout: config.timeout
     });
 
     // Prevent flooding; no more than 2 pending messages. We start at
     // zero as this is called before the first item is queued.
-    if (queue.queue().length >= 2) {
+    if (queue.queue().length >= config.maxPending) {
       return;
     }
     // Currently only one message at the time. Allowing showing multiple
@@ -65,4 +116,5 @@
       render(html, options, next);
     });
   };
+
 })(jQuery);
