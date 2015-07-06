25f7819c215d7e5d3824628dfb0b4f37ba10b087
David Persson <davidpersson@gmx.de>
2015-07-06 12:50:35 +0200

    Reimplement.

--- notify.js
+++ notify.js
@@ -8,37 +8,125 @@
  */
 (function($) {
 
+  // All possible transition event names.
+  var TRANSITION_EV = 'transitionend mozTransitionEnd webkitTransitionEnd oTransitionEnd MSTransitionEnd';
+
   // Global configuration.
   var config = {
     // Selector of the container in which messages will be added.
     container: '#messages',
 
-    // Element used for a message.
-    element: '<div class="message hide"></div>',
+    // The element definition of a message.
+    element: '<div class="message invisible"></div>',
 
     // Number of maximum pending messages, all other messages
     // will be discarded to prevent flooding.
     maxPending: 2,
 
     // Base timeout until a message will be removed after it's shown.
-    timeout: 2100
+    timeout: 2100,
+
+    // Show function used when a message should appear; must return a
+    // promise which should be resolved when i.e. the show animation
+    // finished.
+    show: function($m, $c) {
+      var dfr = new $.Deferred();
+
+      if (typeof Modernizr !== 'undefined' && Modernizr.csstransitions) {
+        if ($m.css('transition-duration')) {
+          $m.one(TRANSITION_EV, dfr.resolve);
+        } else if ($c.css('transition-duration')) {
+          $c.one(TRANSITION_EV, dfr.resolve);
+        } else {
+          dfr.resolve();
+        }
+        $m.removeClass('invisible');
+      } else {
+        $m.fadeIn(dfr.resolve);
+      }
+      return dfr.promise();
+    },
+
+    // Hide function used when a message should disappear; must return
+    // a promise which should be resolved when i.e. the hide animation
+    // finished.
+    hide: function($m, $c) {
+      var dfr = new $.Deferred();
+
+      if (typeof Modernizr !== 'undefined' && Modernizr.csstransitions) {
+        if ($m.css('transition-duration')) {
+          $m.one(TRANSITION_EV, dfr.resolve);
+        } else if ($c.css('transition-duration')) {
+          $c.one(TRANSITION_EV, dfr.resolve);
+        } else {
+          dfr.resolve();
+        }
+        $m.addClass('invisible');
+      } else {
+        $m.fadeOut(dfr.resolve);
+      }
+      return dfr.promise();
+    }
   };
 
   // Our own personal queue.
   var queue = $({});
 
+  // Renders a message and calls complete callback after doing that.
+  var render = function(html, options, complete) {
+    var $m = $(config.element).html(html);
+    var $c = $(config.container);
+
+    if (options.level) {
+      $m.addClass(options.level);
+    }
+    if (options.sticky) {
+      $m.addClass('sticky');
+    }
+    $c.append($m);
+
+    $m.click(function(ev) {
+      config.hide($m, $c).done(function() {
+        $m.remove();
+        complete();
+      });
+    });
+
+    if (options.sticky) {
+      return; // Do not continue and probably show next message.
+    }
+
+    // Multiply the timeout for complex messages, giving comprehension time.
+    // Give 1s per 30 chars over the first 25 chars.
+   var timeout = options.timeout + 1000 * (($($.parseHTML(html)).text().length - 25) / 30);
+
+    // Workaround to enable CSS transitions on dynamically inserted elements.
+    // Reading random property to force recalculating styles.
+    $m.css('top');
+
+    config.show($m, $c).done(function() {
+      setTimeout(function() {
+        config.hide($m, $c).done(function() {
+          $m.remove();
+          complete(); // Show next message in queue.
+        });
+      }, timeout);
+    });
+  };
+
   // Main plugin function. Allows for adding a notification and
   // configuring the plugin globally.
   $.notify = function(html, options) {
+    // Allow setting global configuration for this plugin.
     if (typeof html === 'object') {
-      config = $.extend(html, config);
+      config = $.extend(config, html);
       return;
     }
-    options = $.extend(options || {}, {
+    options = $.extend({
       level: null,
       sticky: false,
       timeout: config.timeout
-    });
+    }, options || {});
 
     // Prevent flooding; no more than 2 pending messages. We start at
     // zero as this is called before the first item is queued.
@@ -51,63 +139,5 @@
       render(html, options, next);
     });
   };
-
-  // Renders a message and calls complete callback after doing that.
-  function render(html, options, complete) {
-    var $m = $(config.element).html(html);
-
-    if (options.level) {
-      $m.addClass(level);
-    }
-    if (options.sticky) {
-      $m.addClass('sticky');
-    }
-    $(config.container).append($m);
-
-    if (options.sticky) {
-      $m.click(function(ev) {
-        toggle($m).done(function() {
-          $m.remove();
-          complete();
-        });
-      });
-      return; // Do not continue and probably show next message.
-    }
-
-    // Multiply the timeout for complex messages, giving comprehension time.
-    // Give 1s per 30 chars over the first 25 chars.
-    options.timeout += 1000 * (($($.parseHTML(html)).text().length - 25) / 30);
-
-    // Workaround to enable CSS transitions on dynamically inserted elements.
-    // Reading random property to force recalculating styles. Has no effect
-    // when transitions aren't used.
-    $m.css('top');
-
-    toggle($m).done(function() {
-      setTimeout(function() {
-        toggle($m).done(function() {
-          $m.remove();
-          complete(); // Show next message in queue.
-        });
-      }, options.timeout);
-    });
-  }
-
-  // Show/hide function used when a message should dis-/appear; must return
-  // a promise which should be resolved when i.e. the show animation
-  // finished.
-  function toggle($m) {
-    var dfr = new $.Deferred();
-
-    // Determines if CSS transitions should/can be used.
-    if (typeof Modernizr !== 'undefined' && Modernizr.csstransitions) {
-      $m.one('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', dfr.resolve);
-    } else {
-      dfr.resolve();
-    }
-    $m.toggleClass('hide');
-
-    return dfr.promise();
-  }
 
 })(jQuery);
